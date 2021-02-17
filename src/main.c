b129bc5596eba176de74c5880a81d70c7f336589
Yanis Van Eeckhout <yanis.vaneeckhout@ugent.be>
2021-02-17 16:23:08 +0100

    3_1_2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+//Binary to gray code
+char binary_gc(unsigned char binary) {
+    return binary ^ (binary >> 1);
+}
+
 int main(void)
 {
     // make the LED pin an output for PORTA
@@ -12,20 +17,23 @@ int main(void)
     PORTE = _BV(PE4)|_BV(PE6);      //Set pin 4 and 6 as pull up
 
     char state_w = 0;               //Initiate W button pressed as 0
-
+    unsigned char counter = 0x00; // Initiate counter at 0
     while (1)
     {
         _delay_ms(100);                 //Delay clock with 100ms
-        if(~PINE & _BV(PE4)) {          // If west button is pressed
+        if(~PINE & _BV(PE4)) {          //If west button is pressed
             if (!state_w) {             //Check if west button has already been pressed
-                if (PORTA == 0xff) {
+                counter += 1;        //Add 1 to counter when button is pressed
+                if (counter == 16) {    //Check if counter is equal to 16 => reset
                     PORTA = 0;
+                    counter = 0;
                 }
-                PORTA = (PORTA << 1) + 1;      //Switch LED7
+
+                PORTA = counter;        //Count with leds
                 state_w = 1;            //Set west button state to pressed
             }
         } else {
-            state_w = 0;
+            state_w = 0;                //Set west button state to unpressed
         }
 
     }
